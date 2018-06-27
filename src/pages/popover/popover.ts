e69648a515c235aeb818a1be5e53bd774a99f2c5
LAPTOP-JHLSCDA7\steve <steve@soaconsultingservices.com>
2018-06-27 14:31:36 -0400

    clean recordings

--- src/pages/popover/popover.ts
+++ src/pages/popover/popover.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { ViewController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {ViewController} from 'ionic-angular';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {NavController, NavParams} from 'ionic-angular';
 
@@ -8,9 +8,7 @@ import {NavController, NavParams} from 'ionic-angular';
   templateUrl: 'popover.html'
 })
 export class PopoverPage {
-
   popoverForm: FormGroup;
-
   name: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, private formBuilder: FormBuilder) {
@@ -21,10 +19,8 @@ export class PopoverPage {
 
   }
 
-  onSubmit(f: any){
+  onSubmit(f: any) {
     console.log("Submitting popover form with value= " + JSON.stringify(f));
     this.viewCtrl.dismiss(f);
   }
-
-
 }
